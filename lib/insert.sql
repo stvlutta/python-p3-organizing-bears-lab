082a470e4fc4fc17fb10844071d5214adde8324c
Stvlutta <stvlutta@gmail.com>
2025-05-16 13:03:20 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 20, "M", "dark brown", "calm", 1),
+("Rowdy", 10, "M", "black", "intense", 1),
+("Tabitha", 6, "F", "brown", "gentle", 1),
+("Sergeant Brown", 19, "M", "brown", "strict", 0),
+("Melissa", 13, "F", "honey", "sweet", 1),
+("Grinch", 2, "M", "green", "grumpy", 1),
+("Wendy", 6, "F", "blonde", "friendly", 1),
+(NULL, 1, "M", "black", "playful", 1);
